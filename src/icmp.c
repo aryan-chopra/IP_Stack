cf36062ec17570b3aaac4c51ec8915e4ed3f72bb
aryan-chopra <aryan.chopra1211@gmail.com>
2024-09-06 11:12:59 +0530

    Documented icmp.c

--- src/icmp.c
+++ src/icmp.c
@@ -1,9 +1,26 @@
+/**
+ * @file icmp.c
+ * @author Aryan Chopra
+ * @brief Replies to an incoming ICMP Request over a network
+ */
+
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "icmp.h"
 #include "ip.h"
+
+/**
+ * @brief Handles the incoming ICMP Reqeust.
+ *
+ *
+ * Extracts the ICMP information from the incoming IP Header.
+ * Checks if the request type is Echo, and calls the appropriate function to modify the incoming header accordingly.
+ *
+ * @param[in, out] ipHeader A struct containing aptly named and sized fields of an IP Header.
+ * If the request type is Echo, calls the appropriate function to modify the incmoing header.
+ */
 
 void handleIcmp(IpHeader *ipHeader) {
   Icmp *icmpInfo = (Icmp *) ipHeader->data;
@@ -19,6 +36,16 @@ void handleIcmp(IpHeader *ipHeader) {
       return;
   }
 }
+
+/**
+ * @brief Changes the info type from Echo to Reply, and recomputes the checksum.
+ *
+ *
+ * @param[in, out] icmpInfo A struct formated with appropriate names and sizes for an ICMP Header.
+ * Changes the type from Echo to Reply to reply back to the ICMP(commonly a ping) request.
+ * Recomputes the checksum.
+ * @param[in] icmpLength Conatains length of ICMP Header.
+ */
 
 void structureIcmpReply(Icmp *icmpInfo, int icmpLength) {
   icmpInfo->type = ICMP_REPLY;
